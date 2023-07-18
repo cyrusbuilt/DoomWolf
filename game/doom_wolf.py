ccd5f82deb03268c73ec9f60f6ea8ef7d4f41abd
Chris Brunner <cyrusbuilt@gmail.com>
2023-07-18 15:00:53 -0400

    Fix enemy inclusion

--- game/doom_wolf.py
+++ game/doom_wolf.py
@@ -60,7 +60,7 @@ class DoomWolf(Game):
                 path = self.maps[self.current_map_index]
                 self.map = game_map(self, path).build()
                 if self.map.enemies:
-                    self.object_handler.enemy_list = self.map.enemies
+                    self.object_handler.enemy_names = self.map.enemies
 
                 s_map = self.map.sprite_map_path
                 if s_map and os.path.exists(s_map):
